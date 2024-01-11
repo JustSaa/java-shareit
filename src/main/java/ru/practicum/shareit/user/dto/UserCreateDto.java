997bf8ead2fb002cc2b40e320992c424e843306f
Salavat <Justdrumm@yandex.ru>
2024-01-11 20:26:20 +0300

    refactor: Checkstyle

--- src/main/java/ru/practicum/shareit/user/dto/UserCreateDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserCreateDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.user.controller.Create;
-import ru.practicum.shareit.user.controller.Update;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
