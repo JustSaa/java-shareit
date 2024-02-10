147296802fc9b08b5c5efc737736b54dc5cdff7b
Salavat <Justdrumm@yandex.ru>
2024-02-10 19:12:20 +0300

    refactor: Checkstyle

--- server/src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ server/src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
